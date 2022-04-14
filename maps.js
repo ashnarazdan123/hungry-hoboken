3f3bd7ee44272d88734ce87195736d3489d79bf8
ashnarazdan123 <65045279+ashnarazdan123@users.noreply.github.com>
2022-04-13 22:02:22 -0400

    map changed to places with details

--- maps.js
+++ maps.js
@@ -2,16 +2,49 @@
 // parameter when you first load the API. For example:
 // Initialize and add the map
 function initMap() {
-    // The location of stevens
-    const stevens = { lat: 40.7433, lng: -74.0266 };
-    // The map, centered at stevens
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: stevens,
+        center: { lat: 40.7433, lng: -74.0266 },
+        zoom: 15,
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: stevens,
-        map: map,
+    const request = {
+        placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
+        fields: ["name", "formatted_address", "place_id", "geometry"],
+    };
+    const infowindow = new google.maps.InfoWindow();
+    const service = new google.maps.places.PlacesService(map);
+
+    service.getDetails(request, (place, status) => {
+        if (
+            status === google.maps.places.PlacesServiceStatus.OK &&
+            place &&
+            place.geometry &&
+            place.geometry.location
+        ) {
+            const marker = new google.maps.Marker({
+                map,
+                position: place.geometry.location,
+            });
+
+            google.maps.event.addListener(marker, "click", () => {
+                const content = document.createElement("div");
+                const nameElement = document.createElement("h2");
+
+                nameElement.textContent = place.name;
+                content.appendChild(nameElement);
+
+                const placeIdElement = document.createElement("p");
+
+                placeIdElement.textContent = place.place_id;
+                content.appendChild(placeIdElement);
+
+                const placeAddressElement = document.createElement("p");
+
+                placeAddressElement.textContent = place.formatted_address;
+                content.appendChild(placeAddressElement);
+                infowindow.setContent(content);
+                infowindow.open(map, marker);
+            });
+        }
     });
 }
+
