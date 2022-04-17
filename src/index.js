48729861d2bdcccdcdbf1d749763863dc983a93c
Andrew Quinlan <andrewquinlan22@gmail.com>
2022-04-17 00:28:05 -0400

    added function to load restaurant data (not complete)

--- src/index.js
+++ src/index.js
@@ -36,20 +36,81 @@ if (testbutton) {
     }, false);
 }
 
-// Test function using Firebase
+// References to restaurants conforming to each dietary restriction
 const allRestaurantsRef = ref(db, 'restaurants');
 // const glutenFreeRestaurantsRef = query(ref(db, 'restaurants'), equalTo('yes', 'glutenFree'), equalTo('partially', 'glutenFree'));
 
-onValue(allRestaurantsRef, (snapshot) => {
-    snapshot.forEach((childSnapshot) => {
-        // const childKey = childSnapshot.key;
-        // const childData = childSnapshot.data;
-        console.log(childSnapshot.key);
+// General function to load restaurant data given a reference
+function listRestaurants1(ref) {
+    console.log("inside listRestaurants");
+    var totalList = ``;
+    onValue(ref, (snapshot) => {
+        console.log("inside onValue");
+        snapshot.forEach((childSnapshot) => {
+            console.log("inside snapshot.forEach");
+            totalList += `<div class="row">`;
+            childSnapshot.forEach((grandchildSnapshot) => {
+                console.log("inside childSnapshot.forEach");
+                if (grandchildSnapshot.key === 'imageURL') {
+                    console.log("inside if imageURL");
+                    totalList += `<img class="logo" src="${grandchildSnapshot.data}">`;
+                } else if (grandchildSnapshot.key === 'name') {
+                    console.log("inside if name");
+                    totalList += `<p>Name:${childSnapshot.data}</p>`;
+                }
+            });
+            totalList += `</div>`;
+            totalList += `<hr>`;
+        });
+    }, {
+        onlyOnce: true
     });
-}, {
-    onlyOnce: true
-});
+    return totalList;
+}
 
+// 2nd attempt at loading restaurant data given a reference
+function listRestaurants2(ref) {
+    document.getElementById("searchResult").innerHTML = '';
+    onValue(ref, (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            console.log(childSnapshot.key);
+
+            const _div = document.createElement("div", {class: 'row'});
+            const _img = document.createElement("img", {class: 'logo', src: `https://upload.wikimedia.org/wikipedia/commons/a/a1/Mallard2.jpg`});
+            const _name = document.createElement("p");
+            _name.innerHTML = childSnapshot.key;
+            const _hr = document.createElement("hr");
+            
+            _div.appendChild(_img);
+            _div.appendChild(_name);
+
+            document.getElementById("searchResult").appendChild(_div);
+            document.getElementById("searchResult").appendChild(_hr);
+        })
+    })
+}
+
+// Test function: load all restaurant data when Vegan button pressed
+var veganButton = document.getElementById("vegan");
+if (veganButton) {
+    veganButton.addEventListener("click", function() {
+        // document.getElementById("searchResult").innerHTML = listRestaurants(allRestaurantsRef);
+        listRestaurants2(allRestaurantsRef);
+    }, false);
+}
+
+// Test function: logs all restaurant names
+// onValue(allRestaurantsRef, (snapshot) => {
+//     snapshot.forEach((childSnapshot) => {
+//         // const childKey = childSnapshot.key;
+//         // const childData = childSnapshot.data;
+//         console.log(childSnapshot.key);
+//     });
+// }, {
+//     onlyOnce: true
+// });
+
+// Test function: log all restaurants reference when Gluten Free button pressed
 var glutenFreeButton = document.getElementById("glutenFree");
 if (glutenFreeButton) {
     glutenFreeButton.addEventListener("click", function() {
